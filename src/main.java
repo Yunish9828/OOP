05d570c5a111211ffd8253e21faec1012b3d6c44
[yunistha2019@gmail.com] <yunistha2019@gmail.com>
2024-08-13 13:42:02 +0545

    OOP

---
+++ src/main.java
@@ -0,0 +1,11 @@
+import models.Mammalia;
+
+public class main {
+    public static void main(String[] args) {
+        Mammalia cow = new Mammalia();
+        cow.getHello();
+
+        Mammalia dog = new Mammalia();
+        dog.getBye();
+    }
+}
---
+++ src/models/Mammalia.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Mammalia {
+    //properties
+    public int noOfChambers = 4;
+    public boolean hairyBody = true;
+
+    //methods
+    public void getHello(){
+        System.out.println("Hello There!");
+    }
+    public void getBye(){
+        System.out.println("Bye There!");
+    }
+}
